3f2bb46cca244e01ccd487ba34bde4e5e812411d
jamesD33064 <james0120160379@gmail.com>
2022-05-18 14:08:51 +0800

    .

--- db/coon.php
+++ db/coon.php
@@ -4,7 +4,7 @@
     $password="";
     // $dbuser="u558221944_ash";
     // $password="Ash20220411";
-    $dbname="u558221944_DESSERT";
+    $dbname="1102systemdesign";
 
 
     $link = mysqli_connect("localhost" , $dbuser ,  $password , $dbname) or die("還沒連上資料庫喔：Ｄ");
